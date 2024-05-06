5d26e5cf8e29a581454ab064e75a3c6bd7f9381d
Duskmage2009 <Duskmage2009@gmail.com>
2024-05-06 12:03:59 +0300

    -Regex to email validation

--- src/main/java/com/github/duskmage2009/userservice2/exeption/UserNotFoundException.java
+++ src/main/java/com/github/duskmage2009/userservice2/exeption/UserNotFoundException.java
@@ -2,6 +2,6 @@ package com.github.duskmage2009.userservice2.exeption;
 
 public class UserNotFoundException extends RuntimeException {
     public UserNotFoundException(Long id) {
-        super(String.format("User not found by id =%s",id));
+        super(String.format("User not found by id = %s", id));
     }
 }
